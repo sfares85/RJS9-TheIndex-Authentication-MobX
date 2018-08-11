28223b79b4e5d9bd15eda3fb6874232bdac3a0c3
Octowl <alsaff1987@gmail.com>
2018-08-12 00:33:50 +0300

    Use the author store

--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -1,42 +1,27 @@
 import React, { Component } from "react";
+import { observer } from "mobx-react";
 
 // Components
 import AuthorCard from "./AuthorCard";
 import SearchBar from "./SearchBar";
 
+// Store
+import authorStore from "./stores/AuthorStore";
+
 class AuthorsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filteredAuthors: this.props.authors
-    };
-
-    this.filterAuthors = this.filterAuthors.bind(this);
-  }
-
-  filterAuthors(query) {
-    query = query.toLowerCase();
-    let filteredAuthors = this.props.authors.filter(author => {
-      return `${author.first_name} ${author.last_name}`
-        .toLowerCase()
-        .includes(query);
-    });
-    this.setState({ filteredAuthors });
-  }
-
   render() {
-    const authorCards = this.state.filteredAuthors.map(author => (
+    const authorCards = authorStore.filteredAuthors.map(author => (
       <AuthorCard key={author.first_name + author.last_name} author={author} />
     ));
 
     return (
       <div className="authors">
         <h3>Authors</h3>
-        <SearchBar changeHandler={this.filterAuthors} />
+        <SearchBar store={authorStore} />
         <div className="row">{authorCards}</div>
       </div>
     );
   }
 }
 
-export default AuthorsList;
+export default observer(AuthorsList);
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,29 +1,18 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { observer } from "mobx-react";
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { query: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ query: event.target.value });
-    this.props.changeHandler(event.target.value);
-  }
-
   render() {
+    const store = this.props.store;
     return (
       <div className="form-group col-lg-6 col-12 mx-auto">
         <div className="input-group my-3">
           <input
             className="form-control"
             type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
+            onChange={e => (store.query = e.target.value)}
           />
           <div className="input-group-append">
             <span className="input-group-text">
@@ -36,4 +25,4 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar;
+export default observer(SearchBar);
