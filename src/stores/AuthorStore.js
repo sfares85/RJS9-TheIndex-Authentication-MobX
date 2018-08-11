23ffbb3281343033e8692ab33eaa9a6536091bb1
Octowl <alsaff1987@gmail.com>
2018-08-11 21:30:56 +0300

    Add an author store

---
+++ src/stores/AuthorStore.js
@@ -0,0 +1,47 @@
+import { decorate, observable, computed } from "mobx";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://the-index-api.herokuapp.com"
+});
+
+class AuthorStore {
+  constructor() {
+    this.authors = [];
+    this.loading = true;
+  }
+
+  fetchAuthors() {
+    return instance
+      .get("/api/authors/")
+      .then(res => res.data)
+      .then(authors => {
+        this.authors = authors;
+        this.loading = false;
+      })
+      .catch(err => console.error(err));
+  }
+
+  get filteredAuthors() {
+    return this.authors.filter(author =>
+      `${author.first_name} ${author.last_name}`
+        .toLowerCase()
+        .includes(this.query)
+    );
+  }
+
+  getAuthorById(id) {
+    return this.authors.find(author => +author.id === +id);
+  }
+}
+
+decorate(AuthorStore, {
+  authors: observable,
+  loading: observable,
+  filteredAuthors: computed
+});
+
+const authorStore = new AuthorStore();
+authorStore.fetchAuthors();
+
+export default authorStore;
