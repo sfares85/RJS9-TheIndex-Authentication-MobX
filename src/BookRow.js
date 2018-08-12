f361badeafd2b7edfac2115b03ef932e375d8a0c
Octowl <alsaff1987@gmail.com>
2018-08-12 16:21:50 +0300

    Wire borrow button

--- src/BookRow.js
+++ src/BookRow.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { observer } from "../node_modules/mobx-react";
 
 class BookRow extends Component {
   render() {
@@ -12,9 +13,7 @@ class BookRow extends Component {
     const availableButton = (
       <button
         className={`btn btn-${book.available ? "success" : "danger"}`}
-        onClick={() =>
-          alert("You need to make me 🎵 work work work work work 🎵")
-        }
+        onClick={() => (book.available = !book.available)}
       >
         {book.available ? "borrow" : "return"}
       </button>
@@ -34,4 +33,4 @@ class BookRow extends Component {
   }
 }
 
-export default BookRow;
+export default observer(BookRow);
